cfa59bfddfc6bbae4b109bfb6dd7679739693ae0
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2022-08-15 17:31:19 +0300

    Cosmas-Push

--- NGO/scripts/script.js
+++ NGO/scripts/script.js
@@ -1,13 +1,37 @@
-const humberger = document.querySelector('.humberger');
-const navMenu = document.querySelector('.nav-menu');
+// const humberger = document.querySelector('.humberger');
+// const navMenu = document.querySelector('.nav-menu');
 
-humberger.addEventListener('click', () => {
-  humberger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+// humberger.addEventListener('click', () => {
+//   humberger.classList.toggle('active');
+//   navMenu.classList.toggle('active');
+// });
+// document.querySelectorAll('.nav-link').forEach(n =>
+//   n.addEventListener('click', () => {
+//     humberger.classList.remove('active');
+//     navMenu.classList.remove('active');
+//   })
+// );
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    1100:{
+      slidesPerView: 3,
+    }
+  }
 });
-document.querySelectorAll('.nav-link').forEach(n =>
-  n.addEventListener('click', () => {
-    humberger.classList.remove('active');
-    navMenu.classList.remove('active');
-  })
-);
